d2655c39c6780aa4f84d230dfee2cfb118235e24
Arif Mahmood <arif.mahmood@pathao.com>
2023-11-20 04:20:33 +0600

    percentage shown 1

--- src/components/monitorHistogram.js
+++ src/components/monitorHistogram.js
@@ -3,75 +3,69 @@ import config from '../../config.yaml'
 import MonitorDayAverage from './monitorDayAverage'
 import StatusCard from "./statusCard";
 
-export default function MonitorHistogram({ monitorId, kvMonitor, dayFilter }) {
-  // create date and set date - daysInHistogram for the first day of the histogram
-  let date = new Date()
-  date.setDate(date.getDate() - dayFilter)
+export default function MonitorHistogram({monitorId, kvMonitor, dayFilter}) {
+    // create date and set date - daysInHistogram for the first day of the histogram
+    let date = new Date()
+    date.setDate(date.getDate() - dayFilter)
 
-  let content = null
+    let content = null
 
-  if (typeof window !== 'undefined') {
-    content = Array.from(Array(dayFilter).keys()).map(
-      (key) => {
-        date.setDate(date.getDate() + 1)
-        const dayInHistogram = date.toISOString().split('T')[0]
+    if (typeof window !== 'undefined') {
+        content = Array.from(Array(dayFilter).keys()).map(
+            (key) => {
+                date.setDate(date.getDate() + 1)
+                const dayInHistogram = date.toISOString().split('T')[0]
 
-        let bg = ''
-        let dayInHistogramLabel = config.settings.dayInHistogramNoData
+                let bg = ''
+                let dayInHistogramLabel = config.settings.dayInHistogramNoData
 
-        // filter all dates before first check, then check the rest
-        if (kvMonitor && kvMonitor.firstCheck <= dayInHistogram) {
-          if (
-            kvMonitor.checks.hasOwnProperty(dayInHistogram) &&
-            kvMonitor.checks[dayInHistogram].fails > 0
-          ) {
-            bg = 'yellow'
-            dayInHistogramLabel = `${kvMonitor.checks[dayInHistogram].fails} ${config.settings.dayInHistogramNotOperational}`
-          } else {
-            bg = 'green'
-            dayInHistogramLabel = config.settings.dayInHistogramOperational
-          }
-        }
+                // filter all dates before first check, then check the rest
+                if (kvMonitor && kvMonitor.firstCheck <= dayInHistogram) {
+                    if (
+                        kvMonitor.checks.hasOwnProperty(dayInHistogram) &&
+                        kvMonitor.checks[dayInHistogram].fails > 0
+                    ) {
+                        bg = 'yellow'
+                        dayInHistogramLabel = `${kvMonitor.checks[dayInHistogram].fails} ${config.settings.dayInHistogramNotOperational}`
+                    } else {
+                        bg = 'green'
+                        dayInHistogramLabel = config.settings.dayInHistogramOperational
+                    }
+                }
 
-        return (
-          <div key={key} className="hitbox tooltip">
-            <div className={`${bg} bar`} />
-            <div className="content text-center py-1 px-2 mt-2 left-1/2 -ml-20 w-40 text-xs">
-              {dayInHistogram}
-              <br />
-              <span className="font-semibold text-sm">
+                return (
+                    <div key={key} className="hitbox tooltip">
+                        <div className={`${bg} bar`}/>
+                        <div className="content text-center py-1 px-2 mt-2 left-1/2 -ml-20 w-40 text-xs">
+                            {dayInHistogram}
+                            <br/>
+                            <span className="font-semibold text-sm">
                 {dayInHistogramLabel}
               </span>
-              {kvMonitor &&
-                kvMonitor.checks.hasOwnProperty(dayInHistogram) &&
-                Object.keys(kvMonitor.checks[dayInHistogram].res).map((key) => {
-                  return (
-                    <MonitorDayAverage
-                      location={key}
-                      avg={kvMonitor.checks[dayInHistogram].res[key].a}
-                    />
-                  )
-                })}
-            </div>
-          </div>
+                            {kvMonitor &&
+                                kvMonitor.checks.hasOwnProperty(dayInHistogram) &&
+                                Object.keys(kvMonitor.checks[dayInHistogram].res).map((key) => {
+                                    return (
+                                        <MonitorDayAverage
+                                            location={key}
+                                            avg={kvMonitor.checks[dayInHistogram].res[key].a}
+                                        />
+                                    )
+                                })}
+                        </div>
+                    </div>
+                )
+            },
         )
-      },
+    }
+
+    return (
+        <div
+            key={`${monitorId}-histogram`}
+            className="flex flex-row items-center histogram">
+            {content}
+        </div>
+
+
     )
-  }
-
-  return (
-      <div className="flex flex-row items-center">
-          <div
-              key={`${monitorId}-histogram`}
-              className="flex flex-row items-center histogram">
-              {content}
-          </div>
-          <div className="flex flex-row">
-              <StatusCard monitorId={monitorId} kvMonitor={kvMonitor} dayFilter={dayFilter}/>
-          </div>
-
-      </div>
-
-
-  )
 }
