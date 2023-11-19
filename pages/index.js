c600716e4c9de4ebf20d428675a647ffd9aa9303
Arif Mahmood <arif.mahmood@pathao.com>
2023-11-20 00:29:41 +0600

    testing 102

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import { Store } from 'laco'
-import { useStore } from 'laco-react'
+import {Store} from 'laco'
+import {useStore} from 'laco-react'
 import Head from 'flareact/head'
 
-import { getKVMonitors, setKVMonitors,  useKeyPress } from '../src/functions/helpers'
+import {getKVMonitors, setKVMonitors, useKeyPress} from '../src/functions/helpers'
 import config from '../config.yaml'
 import MonitorCard from '../src/components/monitorCard'
 import MonitorFilter from '../src/components/monitorFilter'
@@ -11,64 +11,64 @@ import ThemeSwitcher from '../src/components/themeSwitcher'
 import time from 'time'
 
 const MonitorStore = new Store({
-  monitors: config.monitors,
-  visible: config.monitors,
-  activeFilter: false,
+    monitors: config.monitors,
+    visible: config.monitors,
+    activeFilter: false,
 })
 
 const filterByTerm = (term) =>
-  MonitorStore.set((state) => ({
-    visible: state.monitors.filter((monitor) =>
-      monitor.name.toLowerCase().includes(term),
-    ),
-  }))
+    MonitorStore.set((state) => ({
+        visible: state.monitors.filter((monitor) =>
+            monitor.name.toLowerCase().includes(term),
+        ),
+    }))
 
 export async function getEdgeProps() {
-  // get KV data
-  const kvMonitors = await getKVMonitors()
+    // get KV data
+    const kvMonitors = await getKVMonitors()
 
-  return {
-    props: {
-      config,
-      kvMonitors: kvMonitors ? kvMonitors.monitors : {},
-      kvMonitorsLastUpdate: kvMonitors ? kvMonitors.lastUpdate : {},
-      dayFilter: kvMonitors ? kvMonitors.dayFilter : config.settings.daysInHistogram,
-    },
-    // Revalidate these props once every x seconds
-    revalidate: 5,
-  }
+    return {
+        props: {
+            config,
+            kvMonitors: kvMonitors ? kvMonitors.monitors : {},
+            kvMonitorsLastUpdate: kvMonitors ? kvMonitors.lastUpdate : {},
+            dayFilter: kvMonitors ? kvMonitors.dayFilter : config.settings.daysInHistogram,
+        },
+        // Revalidate these props once every x seconds
+        revalidate: 5,
+    }
 }
 
-async function updateDaysInConfig(dayToSet) {
-  // get KV data
-  if (dayToSet !== 30 || dayToSet !== 45 || dayToSet !== 90 || dayToSet !== 120 ){
-    return;
-  }
+const updateDaysInConfig = async (dayToSet) => {
+    // get KV data
+    if (dayToSet !== 30 || dayToSet !== 45 || dayToSet !== 90 || dayToSet !== 120) {
+        return;
+    }
 
-  // const kvMonitorsKey = 'monitors_data_v1_1';
-  // const kvMonitors = await getKVMonitors();
-  // kvMonitors.dayFilter = dayToSet;
-  // await setKVMonitors(kvMonitors);
-  await sleep(5000);
-  location.reload();
+    const kvMonitorsKey = 'monitors_data_v1_1';
+    const kvMonitors = await getKVMonitors();
+    kvMonitors.dayFilter = dayToSet;
+    await setKVMonitors(kvMonitors);
+    await sleep(5000);
+    location.reload();
 }
 
 function sleep(ms = 0) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
-export default function Index({ config, kvMonitors, kvMonitorsLastUpdate, dayFilter }) {
-  const state = useStore(MonitorStore)
-  const slash = useKeyPress('/')
+export default function Index({config, kvMonitors, kvMonitorsLastUpdate, dayFilter}) {
+    const state = useStore(MonitorStore)
+    const slash = useKeyPress('/')
 
-  return (
-    <div className="min-h-screen">
-      <Head>
-        <title>{config.settings.title}</title>
-        <link rel="stylesheet" href="./style.css" />
-        <script>
-          {`
+    return (
+        <div className="min-h-screen">
+            <Head>
+                <title>{config.settings.title}</title>
+                <link rel="stylesheet" href="./style.css"/>
+                <script>
+                    {`
           function setTheme(theme) {
             document.documentElement.classList.remove("dark", "light")
             document.documentElement.classList.add(theme)
@@ -86,51 +86,51 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate, dayFil
             }
           })()
           `}
-        </script>
-      </Head>
-      <div className="container mx-auto px-4">
-        <div className="flex flex-row justify-between items-center p-4">
-          <div className="flex flex-row items-center">
-            <img className="h-8 w-auto" src={config.settings.logo} />
-            <h1 className="ml-4 text-3xl">{config.settings.title}</h1>
-          </div>
-          <div className="flex flex-row items-center">
-            {typeof window !== 'undefined' && <ThemeSwitcher />}
-            <MonitorFilter active={slash} callback={updateDaysInConfig} />
-          </div>
+                </script>
+            </Head>
+            <div className="container mx-auto px-4">
+                <div className="flex flex-row justify-between items-center p-4">
+                    <div className="flex flex-row items-center">
+                        <img className="h-8 w-auto" src={config.settings.logo}/>
+                        <h1 className="ml-4 text-3xl">{config.settings.title}</h1>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        {typeof window !== 'undefined' && <ThemeSwitcher/>}
+                        <MonitorFilter active={slash} callback={updateDaysInConfig}/>
+                    </div>
+                </div>
+                <MonitorStatusHeader kvMonitorsLastUpdate={kvMonitorsLastUpdate}/>
+                {state.visible.map((monitor, key) => {
+                    return (
+                        <MonitorCard
+                            key={key}
+                            monitor={monitor}
+                            data={kvMonitors[monitor.id]}
+                            dayFilter={dayFilter}
+                        />
+                    )
+                })}
+                <div className="flex flex-row justify-between mt-4 text-sm">
+                    <div>
+                        Powered by{' '}
+                        <a href="https://workers.cloudflare.com/" target="_blank">
+                            Cloudflare Workers{' '}
+                        </a>
+                        &{' '}
+                        <a href="https://flareact.com/" target="_blank">
+                            Flareact{' '}
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="https://github.com/eidam/cf-workers-status-page"
+                            target="_blank"
+                        >
+                            Get Your Status Page
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <MonitorStatusHeader kvMonitorsLastUpdate={kvMonitorsLastUpdate} />
-        {state.visible.map((monitor, key) => {
-          return (
-            <MonitorCard
-              key={key}
-              monitor={monitor}
-              data={kvMonitors[monitor.id]}
-              dayFilter={dayFilter}
-            />
-          )
-        })}
-        <div className="flex flex-row justify-between mt-4 text-sm">
-          <div>
-            Powered by{' '}
-            <a href="https://workers.cloudflare.com/" target="_blank">
-              Cloudflare Workers{' '}
-            </a>
-            &{' '}
-            <a href="https://flareact.com/" target="_blank">
-              Flareact{' '}
-            </a>
-          </div>
-          <div>
-            <a
-              href="https://github.com/eidam/cf-workers-status-page"
-              target="_blank"
-            >
-              Get Your Status Page
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
